d67603d9a89337be963679c406b62664d1d45196
Yunus Emre Özgel <75580560+IamYunusEmreOzgel@users.noreply.github.com>
2026-07-20 18:30:09 +0300

    Add Supabase client configuration

---
+++ assets/js/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://vtvmhljzfrcrhreupxfo.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_IgyHOjEWTQ1yYc-3X9v3CQ_hA7vI4zy";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
